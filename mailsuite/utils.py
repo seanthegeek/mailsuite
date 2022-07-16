10de7a4d441adcd9f12d69baff16e203caa297ce
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2022-07-15 23:30:49 -0400

    Fix typo

--- mailsuite/utils.py
+++ mailsuite/utils.py
@@ -575,7 +575,7 @@ def query_dns(domain: str,
 
 def get_reverse_dns(ip_address: str,
                     cache: ExpiringDict = None,
-                    nameservers: Lists[str] = None,
+                    nameservers: List[str] = None,
                     timeout: Union[float, int] = 2.0) -> Union[str, None]:
     """
     Resolves an IP address to a hostname using a reverse DNS query
